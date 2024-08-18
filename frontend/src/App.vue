dad5b964cd41342413b3798e6b04b497e8459e55
iMatheusPaula <imatheuspaula@gmail.com>
2024-08-18 12:14:19 -0300

    vue start

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import Footer from "@/components/Footer.vue";
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <header class="bg-amber-200 w-full flex justify-between">
+  </header>
+  <Footer />
 </template>
