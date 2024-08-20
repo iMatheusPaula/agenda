7dc7c29ac47243f1cf2329d780e11fcd0b935e65
iMatheusPaula <imatheuspaula@gmail.com>
2024-08-20 18:17:36 -0300

    fixing css

--- frontend/src/views/ContactDestroyView.vue
+++ frontend/src/views/ContactDestroyView.vue
@@ -17,22 +17,17 @@ async function handleDestroy(){
   })
 }
 </script>
-
 <template>
   <div class="bg-white rounded-2xl shadow-2xl w-96 lg:w-1/3 justify-center px-6 py-20 lg:px-8">
     <h1 class="text-gray-900 tracking-tight font-bold text-2xl text-center mb-3">Tem certeza que deseja deletar <span class="font-bold">{{ route.params.name }}</span>?</h1>
-    <div class="flex flex-col p-2">
-      <button class="btn" @click="handleDestroy">Deletar</button>
-      <button class="btn" @click="router.back()">Cancelar</button>
+    <div class="flex flex-row">
+      <button class="bg-red-600 hover:bg-red-500" @click="handleDestroy">Deletar</button>
+      <button class="bg-blue-600 hover:bg-blue-500" @click="router.back()">Cancelar</button>
     </div>
   </div>
 </template>
 <style scoped>
-.infos {
-  @apply p-2 m-2 border-0
-  text-gray-900 placeholder:text-gray-500 sm:text-sm sm:leading-6
-}
-.btn{
-  @apply p-2 bg-blue-600 rounded-md mx-2 mt-4 mb-7 hover:bg-blue-500 leading-6 shadow-inner text-white transition duration-200
+button{
+  @apply p-2 rounded-md mx-2 mt-4 shadow-inner text-white transition duration-200 w-full
 }
 </style>
--- frontend/src/views/ContactUpdateView.vue
+++ frontend/src/views/ContactUpdateView.vue
@@ -61,8 +61,10 @@ onMounted(getContact);
       <input id="phone" v-model="state.phone" type="text" placeholder="Telefone" />
       <input id="email" v-model="state.email" type="text" placeholder="E-mail" />
       <input id="image" type="file" @change="handleFileUpload" placeholder="Foto" />
-      <button id="submit-btn" type="submit">Editar</button>
-      <button id="submit-btn" type="button" @click="router.back()">Cancelar</button>
+      <div class="w-full flex flex-row">
+        <button id="submit-btn" type="submit">Salvar</button>
+        <button id="cancel-btn" type="button" @click="router.back()">Cancelar</button>
+      </div>
     </form>
   </div>
 </template>
@@ -71,7 +73,7 @@ form input {
   @apply p-2 bg-gray-100 rounded-md m-2 border-0
   text-gray-900 placeholder:text-gray-500 shadow-inner ring-1 ring-gray-300 sm:text-sm sm:leading-6
 }
-form button{
-  @apply p-2 bg-blue-600 rounded-md mx-2 mt-4 mb-7 hover:bg-blue-500 leading-6 shadow-inner text-white transition duration-200
+form button {
+  @apply p-2 bg-blue-600 rounded-md mx-2 mt-4 hover:bg-blue-500 shadow-inner text-white transition duration-200 w-full
 }
 </style>
